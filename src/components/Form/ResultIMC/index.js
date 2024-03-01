a7dd5171f91df5a624b021133120dd4a468bc206
Nathan Paiva de Lacerda <nathan.lacerda@smartbreeder.com.br>
2024-03-01 15:24:06 -0300

    Estilização - videoAula 7

--- src/components/Form/ResultIMC/index.js
+++ src/components/Form/ResultIMC/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import styles from "../style";
 
 export default function ResultIMC(props) {
     return (
-        <View>
-            <Text>props.messageResultIMC</Text>
-            <Text>props.ResultIMC</Text>
+        <View style={styles.ResultIMC}>
+            <Text style={styles.information}>props.messageResultIMC</Text>
+            <Text style={styles.ResultIMC}>props.ResultIMC</Text>
         </View>
     );
 }
